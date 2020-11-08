7e873f9a03f214dc001b6530e55b1e76e2e44adb
stunnashadez <57342069+stunnashadez@users.noreply.github.com>
2020-11-08 15:04:17 -0500

    Updated Some minor details

--- SI_Project/admin/titleheader.php
+++ SI_Project/admin/titleheader.php
@@ -6,7 +6,7 @@
 <body>
 	<div class="admintitle" align="center">
 		<h4><a href="admindash.php" style="float: left;margin-left: 50px;font-size: 20px;color: white;">Back to Dashboard</a></h4>
-		<h1>Edit Student Details</h1>
+		<h1>Edit Student/Faculty Details</h1>
 	</div>
 </body>
 </html> 
