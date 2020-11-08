a12f8eea6ae5836aeaf4acace90f77e715df67c7
stunnashadez <57342069+stunnashadez@users.noreply.github.com>
2020-11-08 12:25:45 -0500

    Adjusting Verbiage

--- SI_Project/admin/header.php
+++ SI_Project/admin/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Student management System</title>
+	<title>Student Management System</title>
 	<link rel="stylesheet" type="text/css" href="../css/headerstyle.css">
 </head>
 <body>
--- SI_Project/admin/titleheader.php
+++ SI_Project/admin/titleheader.php
@@ -5,8 +5,8 @@
 </head>
 <body>
 	<div class="admintitle" align="center">
-		<h4><a href="admindash.php" style="float: left;margin-left: 50px;font-size: 20px;color: white;">Back to dashboard</a></h4>
-		<h1>Welcome to Admin Dashboard</h1>
+		<h4><a href="admindash.php" style="float: left;margin-left: 50px;font-size: 20px;color: white;">Back to Dashboard</a></h4>
+		<h1>Edit Student Details</h1>
 	</div>
 </body>
 </html> 
