27f3dd987915dbd24f71c488e673b207a5549990
stunnashadez <57342069+stunnashadez@users.noreply.github.com>
2020-11-08 11:20:04 -0500

    Updated index page

--- SI_Project/index.php
+++ SI_Project/index.php
@@ -3,28 +3,27 @@
 <html>
 <head>
          <meta charset="UTF-8">
-	<title>Student information system</title>
+	<title>Student Management System</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 
 </head>
 <body>
 	<header>  
 		<div class="main">
-                    <<h1>Welcome to our Project</h1>
+                    
 			<ul>
 				<li class="active"><a href="#">Home</a></li>
-				<li><a href="#">Departments</a></li>
 				<li><a href="#">Contact</a></li>
 				<li><a href="#">About</a></li>
 				
 			</ul>
 		</div>
 		<div class="title">
-			<h1>Student information Management System</h1>
+			<h1>Student Information Management System</h1>
 		</div>
 		<div class="button">
-                    <a href="studentlogin.php" class="btn">Student login</a>
-			<a href="login.php" class="btn">Admin login</a>
+                    <a href="studentlogin.php" class="btn">Student Login</a>
+			<a href="login.php" class="btn">Admin Login</a>
 			
 		</div>
 	</header>
