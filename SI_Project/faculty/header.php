9fd812e9b595e72ee9ddd8841baefb5fa7137dc2
stunnashadez <57342069+stunnashadez@users.noreply.github.com>
2020-11-08 21:25:55 -0500

    updated faculty dashboard

---
+++ SI_Project/faculty/header.php
@@ -0,0 +1,11 @@
+ <!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Student Management System</title>
+	<link rel="stylesheet" type="text/css" href="../css/headerstyle.css">
+</head>
+<body>
+
+</body>
+</html>
---
+++ SI_Project/faculty/titleheader.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="admintitle" align="center">
+		<h4><a href="admindash.php" style="float: left;margin-left: 50px;font-size: 20px;color: white;">Back to Dashboard</a></h4>
+		<h1>Edit Student/Faculty Details</h1>
+	</div>
+</body>
+</html> 
