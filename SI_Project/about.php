4c77d9976ab867d776ce8396365ac52152576c3c
stunnashadez <57342069+stunnashadez@users.noreply.github.com>
2020-11-08 19:45:22 -0500

    created about page

--- SI_Project/about.php
+++ SI_Project/about.php
@@ -12,20 +12,15 @@
 		<div class="main">
                     
 			<ul>
-				<li class="active"><a href="index.php">Home</a></li>
-				<li><a href="about.php">About</a></li>
+				<li><a href="index.php">Home</a></li>
+				<li class="active"><a href="about.php">About</a></li>
 				
 			</ul>
 		</div>
 		<div class="title">
 			<h1>Student Information Management System</h1>
 		</div>
-		<div class="button">
-            <a href="studentlogin.php" class="btn">Student Login</a>
-			<a href="facultylogin.php" class="btn">Faculty Login</a>
-			<a href="login.php" class="btn">Admin Login</a>
-			
-		</div>
+
 	</header>
     
     
