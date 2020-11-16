ebdf96465995f0698d2eca9b2e6d1a11306522d6
AnuMangat <47878449+AnuMangat@users.noreply.github.com>
2020-11-16 12:04:24 -0500

    fixed the sql

--- SI_Project/faculty/updatedatamarks.php
+++ SI_Project/faculty/updatedatamarks.php
@@ -13,13 +13,9 @@ if(isset($_POST['Submit']))
 	$Mark=$_POST['Mark']; 
 
        
-$sql=("UPDATE `student`.`student_info` SET `StudentName` = '$name',"
-      . "`Major` = '$major', `GPA` = '$gpa' "
-      . "WHERE StudentID = $id");
-
-$sql2=("UPDATE `student`.`enrolled` SET `StudentID` = '$StudentID',"
+$sql=("UPDATE `student`.`enrolled` SET `StudentID` = '$StudentID',"
       . "`CourseID` = '$CourseID', `Mark` = '$Mark' "
-      . "WHERE StudentID = $id");
+      . "WHERE StudentID = {$StudentID} AND CourseID='{$CourseID}'");
 
 
 $result= mysqli_query($db,$sql);   /*include two variable database($db) and query($sql) and finally store $data variable */
