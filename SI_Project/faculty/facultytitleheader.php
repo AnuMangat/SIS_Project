5eb1d61c60d98dd1aafa41a8ac77efa30975fbcc
AnuMangat <47878449+AnuMangat@users.noreply.github.com>
2020-11-17 22:24:15 -0500

    minor_css_change

--- SI_Project/faculty/facultytitleheader.php
+++ SI_Project/faculty/facultytitleheader.php
@@ -4,7 +4,7 @@
 	<title></title>
 </head>
 <body>
-	<div class="admintitle" align="center">
+	<div class="dashboard" align="center">
 		<h4><a href="facultydash.php" style="float: left;margin-left: 50px;font-size: 20px;color: white;">Back to Dashboard</a></h4>
 		<h1>Edit Student Details</h1>
 	</div>
